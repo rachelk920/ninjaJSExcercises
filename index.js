4bd5c57042480e641d6c5b7d5adfc70151c52b55
Rachel <rmshinski@gmail.com>
2021-04-11 07:20:12 -0400

    homework

---
+++ index.js
@@ -0,0 +1,50 @@
+alert("Hello, World!");
+
+alert("alerts suck");
+
+var myVariableShaun;
+
+myVariableShaun = 10;
+
+var myVar2 = "hello";
+
+mvVar2 = "hello again";
+
+myvar2 = 5
+
+var customerAge = 32;
+
+var myVar3 = 40;
+
+var myVar = 5;
+
+myVar + 10;
+
+myVar = myVar + 10;
+
+myVar - 3;
+
+myVar = myVar - 5;
+
+myVar = myVar / 2;
+
+var myVar = 10;
+
+myVar = myVar + 5;
+
+myVar += 5;
+
+myVar -= 10;
+
+myVar /= 2;
+
+myVar *= 5;
+
+myVar += 1;
+
+myVar ++;
+
+++myVar;
+
+var myVar = "Shaun";
+
